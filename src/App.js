52a9f7cc35bb7f3c66897a1312ccd4480b717759
Mauro Meden <mauro.meden@gmail.com>
2022-04-15 12:12:59 -0600

    Add home and jobs routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import Header from "./components/layout/Header";
 import Main from "./components/layout/Main";
 import Footer from "./components/layout/Footer";
--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,7 +1,20 @@
-import react from 'react';
+import React from "react";
+
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Jobs from "../pages/Jobs";
 
 export default function Main() {
-    return <main>
-        Page content
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/jobs" element={<Jobs />} />
+        </Routes>
+      </BrowserRouter>
     </main>
+  );
 }
